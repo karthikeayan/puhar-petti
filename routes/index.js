d1b7ccc044be72490525a3fe1b819440f4927cba
Jagadeesh <karja08@ca.com>
2017-12-17 22:32:22 +0530

    home page with foundation

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Puhar petti' });
 });
 
 module.exports = router;
